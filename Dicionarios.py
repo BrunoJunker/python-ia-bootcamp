f3fc610b9cfb167a40c8a5516ab9c97f68f148f1
Bruno Henrique <brunohmj@hotmail.com>
2026-06-28 10:47:17 -0300

    Listas e dicionarios

---
+++ Dicionarios.py
@@ -0,0 +1,17 @@
+# usuario = {
+#     'nome': 'Bruno',
+#     'idade': 26,
+#     'Departamento':'TI'
+# }
+
+# usuario['idade'] = 20
+# usuario['Cidade'] = 'Joinville'
+
+# print(usuario)
+
+aluno = {
+    'nome': input('Nome do Aluno: '),
+    'idade': input('idade do Aluno: '),
+    'nota': input('Nota do Aluno: ')
+}
+print(aluno)
---
+++ Fundamentos/Listas.py
@@ -0,0 +1,30 @@
+# frutas = ['Abacate', 'Banana', 'morango']
+
+#  print(frutas[0]) # Baseado em index, inicia no zero
+# frutas.append('Uva')
+# frutas.append('Laranja')
+# frutas.remove('Banana')
+# print(len(frutas))
+# print(frutas)
+
+
+# tarefas = []
+
+# tarefas.append('Estudar Python com IA')
+# tarefas.append('Ler um artigo sobre IA')
+# tarefas.append('Responder Emails')
+# tarefas.append('Lavar o carro')
+
+# print('Minhas tarefas de hoje: ')
+# for tarefa in tarefas:
+#     print(f'Tarefa: {tarefa}')
+
+# # print('Minhas tarefas diarias:')
+# # print(tarefas[0])
+# # print(tarefas[1])
+# # print(tarefas[2])
+
+meses = ('Janeiro', 'Fevereriro', 'Marco')
+
+print(meses[1])
+
